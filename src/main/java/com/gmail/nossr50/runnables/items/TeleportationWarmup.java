144d6e5908375591a1113e37527506768fc3559a
t00thpick1 <t00thpick1dirko@gmail.com>
2020-01-03 21:30:33 -0500

    One more check for ptp after warmup

--- src/main/java/com/gmail/nossr50/runnables/items/TeleportationWarmup.java
+++ src/main/java/com/gmail/nossr50/runnables/items/TeleportationWarmup.java
@@ -6,8 +6,10 @@ import com.gmail.nossr50.locale.LocaleLoader;
 import com.gmail.nossr50.party.PartyManager;
 import com.gmail.nossr50.util.EventUtils;
 import com.gmail.nossr50.util.Misc;
+import com.gmail.nossr50.util.Permissions;
 import com.gmail.nossr50.util.skills.SkillUtils;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -48,6 +50,22 @@ public class TeleportationWarmup extends BukkitRunnable {
             if (timeRemaining > 0) {
                 teleportingPlayer.sendMessage(LocaleLoader.getString("Item.Injured.Wait", timeRemaining));
                 return;
+            }
+        }
+
+        if (Config.getInstance().getPTPCommandWorldPermissions()) {
+            World targetWorld = targetPlayer.getWorld();
+            World playerWorld = teleportingPlayer.getWorld();
+
+            if (!Permissions.partyTeleportAllWorlds(teleportingPlayer)) {
+                if (!Permissions.partyTeleportWorld(targetPlayer, targetWorld)) {
+                    teleportingPlayer.sendMessage(LocaleLoader.getString("Commands.ptp.NoWorldPermissions", targetWorld.getName()));
+                    return;
+                }
+                else if (targetWorld != playerWorld && !Permissions.partyTeleportWorld(teleportingPlayer, targetWorld)) {
+                    teleportingPlayer.sendMessage(LocaleLoader.getString("Commands.ptp.NoWorldPermissions", targetWorld.getName()));
+                    return;
+                }
             }
         }
 
